23ee1df0d63a108e8a1a3603bd0c4f59f46289b0
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-09-01 15:37:24 -0400

    worked on index.js created functions for managers questions

--- index.js
+++ index.js
@@ -6,3 +6,40 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
 const generateHTML = require('./src/generateHTML');
+
+//empty array for team profiles
+const teamArr = [];
+
+function manager() {
+    inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team managers name?',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else {
+                    console.log ('Please enter the managers name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the team managers id?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team managers email address?',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the team managers office number?'
+        }
+    ])
+
+}
